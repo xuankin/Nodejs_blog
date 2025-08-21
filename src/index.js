a6fe3e8c8d7f551e0ab29325c7394a17968e8ac7
xuankin <kingnguyen182@gmail.com>
2025-08-21 15:22:08 +0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
